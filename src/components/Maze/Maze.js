0533e148466740af917edfc81ec2ca063816562c
aborgono <albertoiborgonovo@gmail.com>
2023-05-12 11:54:42 -0400

    cleaned up code

--- src/components/Maze/Maze.js
+++ src/components/Maze/Maze.js
@@ -12,43 +12,6 @@ const Maze = (props) => {
     const [playerY, setPlayerY] = useState(0);
     const [score, setScore] = useState(100);
 
-    // const handleKeyDown = (event) => {
-    //     event.preventDefault()
-    //     const { key } = event;
-
-    //     if (key === 'ArrowUp') {
-    //         setPlayerY(playerY - 1);
-    //     } else if (key === 'ArrowDown') {
-    //         setPlayerY(playerY + 1);
-    //     } else if (key === 'ArrowLeft') {
-    //         setPlayerX(playerX - 1);
-    //     } else if (key === 'ArrowRight') {
-    //         setPlayerX(playerX + 1);
-    //     }
-    // };
-
-    // const handleKeyDown = (event) => {
-    //     event.preventDefault();
-    //     const { key } = event;
-    
-    //     if (key === 'ArrowUp' && playerY > 0 && mazeLayout[playerY - 1][playerX] !== 1) {
-    //         setPlayerY(playerY - 1);
-    //     } else if (key === 'ArrowDown' && playerY < mazeLayout.length - 1 && mazeLayout[playerY + 1][playerX] !== 1) {
-    //         setPlayerY(playerY + 1);
-    //     } else if (key === 'ArrowLeft' && playerX > 0 && mazeLayout[playerY][playerX - 1] !== 1) {
-    //         setPlayerX(playerX - 1);
-    //     } else if (key === 'ArrowRight' && playerX < mazeLayout[0].length - 1 && mazeLayout[playerY][playerX + 1] !== 1) {
-    //         setPlayerX(playerX + 1);
-    //     }
-    //   };
-
-    // useEffect(() => {
-    //     document.addEventListener('keydown', handleKeyDown);
-    //     return () => {
-    //         document.removeEventListener('keydown', handleKeyDown)
-    //     }
-    //     })
-        
     return (
         <div>
         <h2 className='maze-game-title'>Maze Game</h2>
