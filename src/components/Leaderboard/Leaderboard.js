8df87dc2035555b024e5f906de9a5aebc5d554b3
Aborgono <albertoiborgonovo@gmail.com>
2023-09-20 17:49:22 -0400

    added styling to leaderboard

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import './Leaderboard.scss';
 
 function LeaderBoard() {
     const [leaderBoard, setLeaderBoard] = useState([])
 
     useEffect(() => {
-        axios.get('http://localhost:8080/users')
+        axios.get('http://localhost:8080/leaderBoard')
         .then((response) => {
             console.log("this is my response", response.data);
             setLeaderBoard(response.data)
@@ -14,15 +15,20 @@ function LeaderBoard() {
 
     console.log("this is my user", leaderBoard);
     return (
-    <div>
+        <div className="leaderboard-container">
         {leaderBoard.map((user) => (
-            <div key={user.id}>
-                <h1>{user.name}</h1>
-                    <p>{user.score}</p>
-            </div>
-        ))
-        }
-    </div>
+          <div key={user.id} className="leaderboard-entry">
+            <h1>{user.name}</h1>
+            <li>
+              <ul>
+                <li>Difficulty: {user.difficulty}</li>
+                <li>Score: {user.score}</li>
+              </ul>
+            </li>
+          </div>
+        ))}
+      </div>
+      
     );
 }
 
