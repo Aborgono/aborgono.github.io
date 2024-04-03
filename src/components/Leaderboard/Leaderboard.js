70c2515f4ac2ef4097f367d25ef7154c46c7ea0e
albertoforditch <alberto@ditch.io>
2024-04-03 13:45:21 -0400

    changed leaderboard to include default values

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -6,39 +6,49 @@ function LeaderBoard() {
 
   const [leaderBoard, setLeaderBoard] = useState([]);
   const [loading, setLoading] = useState(true); // Add loading state
+  const [error, setError] = useState(false); // Add error state
+
 
   useEffect(() => {
-      axios.get('https://vercel-maze-runner-2cbwqwsp1-aborgono.vercel.app/api/leaderboard')
-          .then((response) => {
-              console.log("this is my response", response.data);
-              setLeaderBoard(response.data);
-              setLoading(false); // Data has been loaded, set loading to false
-          })
-          .catch((error) => {
-              console.error("Error loading leaderboard data:", error);
-              setLoading(false); // Even in case of an error, set loading to false
-          });
+    axios.get('https://vercel-maze-runner-2cbwqwsp1-aborgono.vercel.app/api/leaderboard')
+      .then((response) => {
+        console.log("this is my response", response.data);
+        setLeaderBoard(response.data);
+      })
+      .catch((error) => {
+        console.error("Error loading leaderboard data:", error);
+        setError(true); // Set error state to true in case of an error
+      })
+      .finally(() => setLoading(false)); // Set loading to false in both cases
   }, []);
+  
+  console.log("this is my user", leaderBoard);
+  
+ // Conditional rendering based on loading and error states
+ if (loading) return <p>Loading...</p>;
+ if (error) return (
+   <div className="leaderboard-entry">
+     <h1>"Test User: DB has been disabled"</h1>
+     <ul>
+       <li>Difficulty: Easy</li>
+       <li>Score: 100</li>
+     </ul>
+   </div>
+ );
 
-    console.log("this is my user", leaderBoard);
-
-  return (
-    <div className="leaderboard-container">
-      {loading ? (
-          <p>Loading...</p>
-      ) : (
-          leaderBoard.map((user) => (
-              <div key={user.id} className="leaderboard-entry">
-                  <h1>{user.username}</h1>
-                  <ul>
-                      <li>Difficulty: {user.selectedMode}</li>
-                      <li>Score: {user.score}</li>
-                  </ul>
-              </div>
-          ))
-      )}
-    </div>
-  );
+ return (
+   <div className="leaderboard-container">
+     {leaderBoard.map((user) => (
+       <div key={user.id} className="leaderboard-entry">
+         <h1>{user.username}</h1>
+         <ul>
+           <li>Difficulty: {user.selectedMode}</li>
+           <li>Score: {user.score}</li>
+         </ul>
+       </div>
+     ))}
+   </div>
+ );
 }
 
 export default LeaderBoard;
